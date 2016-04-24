80e7f462a4220424baf596134814c6cb0fa56ca3
Jake Mulley <mulley.jake@me.com>
2016-04-24 16:52:21 +0100

    feature(tests): add mocha tests

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+# Changelog
+
+## 1.0.0
+* Initial production-ready version
+* Write Mocha tests
+
+## 0.0.2
+* Fix issue with non-JS and non-CSS files (e.g .map files) being hashed
+* Remove extra AssetInjector references
+
+## 0.0.1
+* Initial working code
+* Split [AssetInjector](https://github.com/jakemulley/assetinjector) from project
+* Implement hashlength option
--- test.js
+++ test.js
@@ -1,7 +1,46 @@
 var expect     = require('chai').expect(),
-    hashbuster = require('./index');
+    hashbuster = require('./index'),
+    fs         = require('fs');
 
-hashbuster({
-  basePaths: ['test/_css', 'test/_js'],
-  hashLength: 6
+var testCss = {
+  'folder': './test/_css',
+  'files': ['screen.8c3532.css', 'screen.css.map']
+};
+var testJs = {
+  'folder': './test/_js',
+  'files': ['test1.6d7a72.js', 'test1.js.map', 'test2.5bb86a.js', 'test2.js.map']
+};
+var testingDirs = [testCss, testJs];
+
+describe('Hashbuster', function() {
+
+  before(function(done) {
+
+    hashbuster({
+      basePaths: ['./test/_css', './test/_js'],
+      hashLength: 6
+    });
+
+    setTimeout(function() {
+      done();
+    }, 500);
+
+  });
+
+  it('should check the filenames have the correct hash', function() {
+
+    var dirResults;
+    for (var i = testingDirs.length - 1; i >= 0; i--) {
+
+      for (var k = testingDirs[i].files.length - 1; k >= 0; k--) {
+
+        var file = testingDirs[i].folder + '/' + testingDirs[i].files[k];
+        fs.statSync(file);
+
+      }
+
+    }
+
+  });
+
 });
